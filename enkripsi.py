2a126e553be1af308e602c8a6e2bd91633365acb
ananthatisaaudrian <58337411+ananthatisaaudrian@users.noreply.github.com>
2022-01-04 00:09:51 +0800

    Delete enkripsi.py

--- enkripsi.py
+++
@@ -1,8 +0,0 @@
-plaintext = input("masukkan plaintext : ")
-key = 0x6c
-ciphertext = ""
-
-for i in plaintext:
-    ciphertext += chr(ord(i)^key)
-print("Chipertext dari",plaintext,"yaitu : ", ciphertext)
-ciphertext
