614b564bf319dabdf2e7d03cc3455108f631f322
ananthatisaaudrian <58337411+ananthatisaaudrian@users.noreply.github.com>
2022-01-04 00:09:42 +0800

    Delete dekripsi.py

--- dekripsi.py
+++
@@ -1,8 +0,0 @@
-ciphertext = input("Masukkan Chipertext : ") 
-key = 0x61
-plaintext= ""
-
-for i in ciphertext:
-    plaintext += chr(ord(i)^key)
-print("Plaintextnya yaitu : ", plaintext)
-plaintext
